a1f9c0462e1039d064cb5fed089ad44ac8d9ab80
Elvira Quiroz <elvira.quiroz.dp@gmail.com>
2020-05-28 01:44:39 +0000

    contador listo

--- test/src/card.js
+++ test/src/card.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 
-function Card() {
+function Card(props) {
     return (
         <div className="card">
             <div className="card-body">
@@ -13,22 +13,22 @@ function Card() {
                         <h1><span className="badge badge-secondary"><i class="far fa-clock fa-1px"></i></span></h1>
                     </div>
                     <div className="col-1">
-                        <span className="badge badge-secondary">0</span>
+                        <span className="badge badge-secondary">{props.number6}</span>
                     </div>
                     <div className="col-1">
-                        <span className="badge badge-secondary">0</span>
+                        <span className="badge badge-secondary">{props.number5}</span>
                     </div>
                     <div className="col-1">
-                        <span className="badge badge-secondary">0</span>
+                        <span className="badge badge-secondary">{props.number4}</span>
                     </div>
                     <div className="col-1">
-                        <span className="badge badge-secondary">0</span>
+                        <span className="badge badge-secondary">{props.number3}</span>
                     </div>
                     <div className="col-1">
-                        <span className="badge badge-secondary">0</span>
+                        <span className="badge badge-secondary">{props.number2}</span>
                     </div>
                     <div className="col-1">
-                        <span className="badge badge-secondary">0</span>
+                        <span className="badge badge-secondary">{props.number1}</span>
                         <div className="col-2"></div>
                     </div>
                 </div>
--- test/src/index.js
+++ test/src/index.js
@@ -5,13 +5,53 @@ import Card from './card'
 import Badge from './badge'
 import 'bootstrap/dist/css/bootstrap.css';
 import css from './index.css'
+import { number, checkPropTypes } from 'prop-types';
 
-function App () {
+function App(props) {
     return (
-        <Card/>
+        <Card number1={props.number1} 
+        number2={props.number2} 
+        number3={props.number3} 
+        number4={props.number4} 
+        number5={props.number5} 
+        number6={props.number6} />
     );
 }
+let number1 = 0;
+let number2 = 0;
+let number3 = 0;
+let number4 = 0;
+let number5 = 0;
+let number6 = 0;
 
 
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+setInterval(() => {
+
+    number1 += 1;
+    if (number1 == 10) {
+        number1 = 0;
+        number2++;
+    } else if (number2 == 10) {
+        number2 = 0
+        number3 ++
+    } else if (number3 == 10) {
+        number3 = 0
+        number4++
+    } else if (number4 == 10) {
+        number4 = 0
+        number5++
+    } else if(number5 == 10) {
+        number5 = 0
+        number6++
+    }
+
+    ReactDOM.render(<App number1={number1} number2={number2} number3={number3} number4={number4} number5={number5} number6={number6}
+
+    />, document.querySelector('#root'));
+
+
+}, 10)
+
+
+
